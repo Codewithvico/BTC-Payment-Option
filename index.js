5f9326ee1713e59f31160b04f94b8241aa0da28c
CodeWithVico <87899682+Codewithvico@users.noreply.github.com>
2021-12-15 02:42:37 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let copyText = document.querySelector(".copy-text");
+copyText.querySelector("button").addEventListener("click", function () {
+	let input = copyText.querySelector("input.text");
+	input.select();
+	document.execCommand("copy");
+	copyText.classList.add("active");
+	window.getSelection().removeAllRanges();
+	setTimeout(function () {
+		copyText.classList.remove("active");
+	}, 2500);
+});
+
+document.getElementById("backBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  window.location.href="#";
+}
+
+window.onload = function(){
+   setInterval(function(){
+       alert("Hello");
+   }, 1000);
+};
+
+
+
+
